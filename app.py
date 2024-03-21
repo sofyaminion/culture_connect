3aeeac8276e6371a1a72f5c5ee96137704ea4281
sofyaminion <sofya.apolosova@yandex.ru>
2024-03-22 00:43:46 +0300

    add css styles

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import db, User, Event, Profile, Like, Message
+from models import db, User, Event, Profile, Like, Message, Admin
 import logging
 from flask_login import login_user, logout_user, LoginManager, login_required, current_user
 import os
@@ -25,42 +25,54 @@ def create_app():
     with app.app_context():
         db.create_all()
 
-        user1 = User.query.filter_by(email='user1@example.com').first()
-        if not user1:
+        if not User.query.filter_by(username='user1').first():
             user1 = User(username='user1', email='user1@example.com')
             user1.set_password('password1')
             db.session.add(user1)
 
-        user2 = User.query.filter_by(email='user2@example.com').first()
-        if not user2:
+        if not User.query.filter_by(username='user2').first():
             user2 = User(username='user2', email='user2@example.com')
             user2.set_password('password2')
             db.session.add(user2)
 
-        user3 = User.query.filter_by(email='user3@example.com').first()
-        if not user3:
+        if not User.query.filter_by(username='user3').first():
             user3 = User(username='user3', email='user3@example.com')
             user3.set_password('password3')
             db.session.add(user3)
 
-        workshop = Event(name='Воркшоп по веб-разработке', date=datetime(2024, 4, 1), location='Место проведения', description='Описание события')
-        conference = Event(name='Конференция по искусственному интеллекту', date=datetime(2024, 5, 15), location='Место проведения', description='Описание события')
-        festival = Event(name='Фестиваль музыки и искусства', date=datetime(2024, 7, 20), location='Место проведения', description='Описание события')
-        db.session.add(workshop)
-        db.session.add(conference)
-        db.session.add(festival)
+        if not Admin.query.filter_by(username='admin').first():
+            admin = Admin(username='admin', email='admin@example.com')
+            admin.set_password('admin')
+            db.session.add(admin)
+
+        if not Event.query.filter_by(name='Воркшоп по веб-разработке').first():
+            workshop = Event(name='Воркшоп по веб-разработке', date=datetime(2024, 4, 1),
+                             location='Место проведения',
+                             description='Описание события')
+            db.session.add(workshop)
+
+        if not Event.query.filter_by(name='Конференция по искусственному интеллекту').first():
+            conference = Event(name='Конференция по искусственному интеллекту', date=datetime(2024, 5, 15),
+                               location='Место проведения', description='Описание события')
+            db.session.add(conference)
+
+        if not Event.query.filter_by(name='Фестиваль музыки и искусства').first():
+            festival = Event(name='Фестиваль музыки и искусства', date=datetime(2024, 7, 20),
+                             location='Место проведения', description='Описание события')
+            db.session.add(festival)
 
         db.session.commit()
 
-# Ваш код обработки запросов и маршрутов
+    # Ваш код обработки запросов и маршрутов
 
     @app.route('/')
     def index():
         return render_template('index.html')
 
     @app.route('/home')
+    @login_required
     def home():
-        return 'Домашняя страница'
+        return render_template('home.html')
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
@@ -136,44 +148,52 @@ def create_app():
         # Получаем текущего пользователя
         current_user_username = current_user.username
 
-        # Проверяем, является ли запрошенный профиль текущим пользователем
-        if username != current_user_username:
-            return 'You can only view your own profile'
-
         # Получаем пользователя из базы данных
         user = User.query.filter_by(username=username).first()
         if user is None:
             return 'User not found'
 
-        # Отображаем профиль пользователя
-        return render_template('profile.html', username=user.username, email=user.email)
+        # Проверяем, является ли текущий пользователь администратором
+        admin = Admin.query.filter_by(username=current_user_username).first()
+        if admin:
+            # Показываем профиль пользователя администратору
+            return render_template('profile.html', username=user.username, email=user.email)
+        else:
+            # Проверяем, является ли запрошенный профиль текущим пользователем
+            if username != current_user_username:
+                return 'You can only view your own profile'
+            else:
+                # Показываем профиль текущего пользователя
+                return render_template('profile.html', username=user.username, email=user.email)
 
     @app.route('/profile/edit/<username>', methods=['GET', 'POST'])
-    def edit_profile():
-        # Получаем текущего пользователя из базы данных
-        # В данном примере я предполагаю, что имя пользователя хранится в сеансе
-        # Вы можете использовать другой механизм для определения текущего пользователя, если у вас он есть
-        username_from_session = request.form.get('username_from_session')  # Получаем имя пользователя из сессии
-        user = User.query.filter_by(username=username_from_session).first()
+    @login_required
+    def edit_profile(username):
+        # Проверяем, имеет ли текущий пользователь право редактировать профиль
+        if current_user.username != username:
+            return 'You are not authorized to edit this profile'
+
+        user = User.query.filter_by(username=username).first()
         if user is None:
             return 'User not found'
 
         if request.method == 'POST':
-            username = request.form['username']
-            email = request.form['email']
+            new_username = request.form['username']
+            new_email = request.form['email']
 
             # Обновляем данные профиля пользователя
-            user.username = username
-            user.email = email
+            user.username = new_username
+            user.email = new_email
             db.session.commit()
 
             # После сохранения изменений перенаправляем пользователя на страницу профиля
-            return redirect(url_for('profile', username=username))
+            return redirect(url_for('profile', username=new_username))
 
         # Если метод запроса GET, отображаем форму редактирования профиля
         return render_template('edit_profile.html', username=user.username, email=user.email)
 
     @app.route('/profile/<username>')
+    @login_required
     def view_profile(username):
         user = User.query.filter_by(username=username).first()
         if user is None:
@@ -181,18 +201,48 @@ def create_app():
         return render_template('view_profile.html', username=user.username, email=user.email)
 
 
-    @app.route('/events')
+    @app.route('/events', methods=['GET', 'POST'])
+    @login_required
     def events():
+
         if request.method == 'POST':
             event_id = request.form.get('event_id')
             event = Event.query.get(event_id)
             if event:
                 event.likes += 1
                 db.session.commit()
+
+        current_user_username = current_user.username
+
+        # Проверяем, является ли текущий пользователь администратором
+        admin = Admin.query.filter_by(username=current_user_username).first()
+
+        # Отображаем список событий
         events = Event.query.all()
-        return render_template('events.html', events=events)
+        return render_template('events.html', events=events, admin=admin)
+
+    @app.route('/add_event', methods=['GET', 'POST'])
+    @login_required
+    def add_event():
+        if request.method == 'POST':
+            # Получаем данные формы добавления события
+            name = request.form['name']
+            date = datetime.strptime(request.form['date'], '%Y-%m-%d').date()
+            location = request.form['location']
+            description = request.form['description']
+
+            # Создаем новое событие и сохраняем его в базе данных
+            new_event = Event(name=name, date=date, location=location, description=description)
+            db.session.add(new_event)
+            db.session.commit()
+
+            flash('Событие успешно добавлено!', 'success')
+            return redirect(url_for('events'))
+
+        return render_template('add_event.html')
 
     @app.route('/events/<event_id>')
+    @login_required
     def event_details(event_id):
         event = Event.query.get(event_id)
         if event is None:
@@ -200,6 +250,7 @@ def create_app():
         return render_template('event_details.html', event=event)
 
     @app.route('/event/<int:event_id>/like/<int:user_id>', methods=['POST'])
+    @login_required
     def like_person(event_id, user_id):
         event = Event.query.get(event_id)
         user = User.query.get(user_id)
@@ -210,14 +261,10 @@ def create_app():
         return redirect(url_for('event_details', event_id=event_id))
 
     @app.route('/search')
+    @login_required
     def search():
         # Код для поиска событий по различным параметрам
         return render_template('search.html')
-
-    @app.route('/events/category/<category_id>')
-    def events_by_category(category_id):
-        # Код для фильтрации событий по категориям
-        return render_template('events_by_category.html')
 
     def match():
         matched_events = []
@@ -238,12 +285,14 @@ def create_app():
         return render_template('match.html', matched_events=matched_events)
 
     @app.route('/dialog/<username>')
+    @login_required
     def dialog(username):
         # Здесь может быть код для открытия диалога с выбранным пользователем
         return f"You are now in dialog with {username}."
 
 
     @app.route('/send_message', methods=['POST'])
+    @login_required
     def send_message():
         data = request.json
         sender_id = data.get('sender_id')
@@ -254,6 +303,7 @@ def create_app():
         return jsonify({'success': True, 'message': 'Message sent successfully'})
 
     @app.route('/get_messages/<int:user_id>', methods=['GET'])
+    @login_required
     def get_messages(user_id):
         # Здесь можно добавить логику для получения сообщений из базы данных
         # Например, запросить все сообщения, где user_id является отправителем или получателем
@@ -262,11 +312,13 @@ def create_app():
         return jsonify({'messages': messages})
 
     @app.route('/like/<int:user_id>', methods=['POST'])
+    @login_required
     def like_user(user_id):
         # Получить текущего пользователя или его сеанс
         return jsonify({'success': True, 'message': 'User liked successfully'})
 
     @app.route('/unlike/<int:user_id>', methods=['POST'])
+    @login_required
     def unlike_user(user_id):
         # Получить текущего пользователя или его сеанс
         return jsonify({'success': True, 'message': 'User unliked successfully'})
--- models.py
+++ models.py
@@ -18,6 +18,7 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -45,6 +46,23 @@ class Event(db.Model):
     date = db.Column(db.Date, nullable=False)
     location = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(255), nullable=True)  # изменение на nullable=True
+
+class Admin(db.Model):
+    __tablename__ = 'admin'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<Admin {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
